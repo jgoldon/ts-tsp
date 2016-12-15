801d6247c92d6a9dbae5ea1c2a6235876aea377c
jgoldon <218300@student.pwr.edu.pl>
2016-12-15 08:15:18 +0100

    Update Solution.cpp

--- tsp/Solution.cpp
+++ tsp/Solution.cpp
@@ -12,49 +12,60 @@ Solution::Solution(Map* map)
 
 void Solution::initSolution() 
 {
-	for (int i = 0; i < map->Dimension; i++) {
+	for (int i = 0; i < map->Dimension; i++) 
+	{
 		this->v[i] = i;
 	}
 	srand(time(NULL));
-	for (int i = (map->Dimension - 1); i >= 0; i--) {
+	for (int i = (map->Dimension - 1); i >= 0; i--) 
+	{
 		int j = rand() % map->Dimension;
 		swap(this->v[i], this->v[j]);
 	}
 	computeScore();
 }
 
-void Solution::computeScore() {
+void Solution::computeScore() 
+{
 	score = 0;
-	for (int i = 0; i < (map->Dimension - 1); i++) {
+	for (int i = 0; i < (map->Dimension - 1); i++) 
+	{
 		score += map->CalculateDistance(this->v[i], this->v[i + 1]);
 	}
 	score += map->CalculateDistance(this->v[map->Dimension - 1], this->v[0]);
 }
 
-void Solution::printPath() {
+void Solution::printPath() 
+{
 	cout << "Path: ";
-	for (int i = 0; i < map->Dimension; i++) {
+	for (int i = 0; i < map->Dimension; i++) 
+	{
 		cout << this->v[i] << " ";
 	}
 	cout << endl;
 }
 
-int Solution::getV(int key) {
+int Solution::getV(int key) 
+{
 	return this->v[key];
 }
-void Solution::set(int key, int value) {
+void Solution::set(int key, int value) 
+{
 	this->v[key] = value;
 }
 
-void Solution::swapSolve(int i, int j) {
+void Solution::swapSolve(int i, int j) 
+{
 	swap(this->v[i], this->v[j]);
 }
 
-double Solution::getScore() {
+double Solution::getScore() 
+{
 	computeScore();
 	return score;
 }
 
-void Solution::free() {
+void Solution::free() 
+{
 	delete[] this->v;
 }
